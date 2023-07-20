92cee667f767fab5854bbea1189f623c7a4134e9
Abhijeet8473 <abhijeetkashid84734@gmail.com>
2023-07-21 01:26:51 +0530

    update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 # import sqlalchemy
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://d5fi33vnr19jzm91wntp:pscale_pw_2vbD904IUP8XgvxjtkFVMCXUnjxLdA61byXy8WJip1v@aws.connect.psdb.cloud/yourscareer?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://94g4yfis14y0h9gzwny9:pscale_pw_4TKOJ78P5S15zQEvYNGHIHHYTv7pLQq3Hy8wEIHNoZp@aws.connect.psdb.cloud/yourscareer?charset=utf8mb4"
 
 engine = create_engine(
     db_connection_string, 
