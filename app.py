bbcecce43c9e7f50d2b7c776c15e09d5e6998207
Abhijeet8473 <abhijeetkashid84734@gmail.com>
2023-07-21 23:05:02 +0530

    final commit

--- app.py
+++ app.py
@@ -1,46 +1,12 @@
-from flask import Flask,render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from flask import Flask,render_template, jsonify, request
+from database import load_jobs_from_db, load_jobs_form_db, add_application_to_db
+
 
 # app is the object of python
+# pass : ixyfhswoksyqcuvq
+
 
 app = Flask(__name__)
-
-# JOBS = [
-#     {
-#         'id' : 1,
-#         'title' : 'Data Analyst',
-#         'location' : 'Bengaluru',
-#         'salary' : 'Rs 1,00,000'
-#     },
-#     {
-#         'id' : 2,
-#         'title' : 'Data Scintist',
-#         'location' : 'Delhi',
-#         'salary' : 'Rs 15,00,000'
-#     },
-#     {
-#         'id' : 3,
-#         'title' : 'Frontend Engineer',
-#         'location' : 'Remote',
-#         'salary' : 'Rs 12,00,000'
-#     },
-#     {
-#         'id' : 4,
-#         'title' : 'Back Engineer',
-#         'location' : 'San Francisco, USA',
-#         'salary' : '$ 120,000'
-#     }
-# ]
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-
-        jobs = []
-        for row in result.all():
-            jobs.append(row._asdict())
-        return jobs
 
 @app.route("/")
 def hello_world():       #define a function
@@ -49,10 +15,52 @@ def hello_world():       #define a function
                            jobs=jobs,
                            company_name='Career')
 
+
+@app.route("/job/<id>")
+def show_job(id):
+    job =load_jobs_form_db(id)
+
+    if not job:
+        return "Not Found", 404
+    
+    return render_template('job_page.html',
+                          job = job)
+
 # instead of returning html files we can also return json file (json = javascript object)
-# @app.route("/api/jobs")
-# def list_jobs():
-#     return jsonify(jobs)
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_jobs(id):
+    data = request.form
+    job = load_jobs_form_db(id)
+    # return jsonify(job['title'])
+    # store the data in db
+    # display an aknowledgement
+    if job:
+        add_application_to_db(id, data)
+        # send_confirmation_email(data, job['title'], data['email'])  # Send the confirmation email
+        return render_template('application_submit.html', job=job, application=data)
+    else:
+        # Handle the case when the job with the given id does not exist
+        # For example, you can redirect to an error page or display an error message
+        return render_template('job_not_found.html')
+
+# application = load_jobs_form_db('email')
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = '578'  # Use the appropriate port for your mail server
+# app.config['MAIL_USERNAME'] = 'codewithme21@gmail,com'
+# app.config['MAIL_PASSWORD'] = 'Abhijeet@2002'
+# # app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = True
+
+# mail = Mail(app)
+
+# def send_confirmation_email(application_data, job_title, recipient_email):
+#     subject = "Job Application Confirmation - " + job_title
+#     body = f"Dear {application_data['full_name']},\n\nThank you for applying to the job '{job_title}'. We have received your application.\n\nBest regards,\nThe Career Team"
+
+#     msg = Message(subject, recipients=[recipient_email], body=body)
+#     mail.send(msg)
+
+
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 # import sqlalchemy
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://94g4yfis14y0h9gzwny9:pscale_pw_4TKOJ78P5S15zQEvYNGHIHHYTv7pLQq3Hy8wEIHNoZp@aws.connect.psdb.cloud/yourscareer?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://zsaid0qcqgth099orlgm:pscale_pw_aWBPCCBvF8YqPaZ5tZupmMJeDTXERwmalj4RpxnQikw@aws.connect.psdb.cloud/yourscareer?charset=utf8mb4"
 
 engine = create_engine(
     db_connection_string, 
@@ -13,6 +13,14 @@ engine = create_engine(
     }
 )
 
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
 # with engine.connect() as conn:
 #     result = conn.execute(text("select * from jobs"))
 
@@ -20,9 +28,30 @@ engine = create_engine(
 #     for row in result.all():
 #         jobs.append(row._asdict())
 #     print(jobs)
-    
 
+def load_jobs_form_db(id):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM jobs WHERE id = :val")
+        result = conn.execute(query.bindparams(val=id))
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            row_dict = rows[0]._asdict()  # Add parentheses to call the method
+            return row_dict
+        
+ 
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO application(job_id, full_name, email, linkedin_url, education, work_Experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, :education, :work_Experience, :resume_url)")
 
-    
-    
-    
+        conn.execute(query, {
+            "job_id": job_id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin_url": data['linkedin_url'],
+            "education": data['education'],
+            "work_Experience": data['work_Experience'],
+            "resume_url": data['resume_url']
+        })
+
